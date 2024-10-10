6b967822c1d5f117a96bd520640257d327112f12
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-10-11 02:13:01 +0500

    changes db images s

--- build/blogcontroladmin/inc/db.php
+++ build/blogcontroladmin/inc/db.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
- // Database credentials local
+// // Database credentials local
 // $servername = getenv('DB_HOST') ?: 'localhost';
 // $username = getenv('DB_USER') ?: 'root';
 // $password = getenv('DB_PASS') ?: '';
